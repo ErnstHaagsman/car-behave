4637245474b41a3cc74b1336b9e22edb1a7673d9
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-06-16 18:51:39 +0200

    Implement Car
    Also, some casts in the test code

--- car.py
+++ car.py
@@ -1,2 +1,131 @@
 class Car:
-    pass
+    STEP_SIZE = 0.1
+
+    # Coefficient of friction for rolling resistance
+    MU_N = 0.015
+
+    # Air density in kg/m^3
+    RHO = 1.2
+
+    # Gravity on earth
+    G = 9.81
+
+    def __init__(self, engine_power=270, weight=1251, drag_coefficient=0.38, frontal_area=1.77, brake_force=10_000):
+        """
+        Create a car to simulate.
+
+        Limitations: The car has infinite grip, and engine power is fully linear (somewhat realistic for electric cars)
+
+        :param engine_power:        The engine's power in kW
+        :param weight:              The car's curb weight in kg
+        :param drag_coefficient:    Cw value (dimensionless)
+        :param frontal_area:        Frontal area of the car in m^2
+        :param brake_force:         Force the brakes can exert in N
+        """
+        # Car status
+        self.power = 0
+        self.braking = 0
+        self.speed = 0
+        self.odometer = 0
+        self.heading = 360
+        self.yaw_rate = 0
+        self.time = 0.0
+
+        # Car characteristics
+        self.engine_power = engine_power
+        self.weight = weight
+        self.drag_coefficient = drag_coefficient
+        self.frontal_area = frontal_area
+        self.brake_force = brake_force
+
+
+
+    def set_power(self, amount):
+        """
+        Sets the power level as a percentage, think of it as how deeply you've pressed the gas pedal
+        :param amount: Required amount of power between 0 and 100 (percent)
+        """
+        self.power = float(amount)
+
+
+    def set_brake(self, amount):
+        """
+        Sets the desired amount of braking. Percentage of full braking force.
+        :param amount: Required amount of brakes between 0 and 100 (percent)
+        """
+        self.braking = float(amount)
+
+
+    def turn_left(self, rate):
+        """
+        Start turning left at the specified rate
+        :param rate: Yaw rate in degrees / second
+        """
+        self.yaw_rate = - float(rate)
+
+
+    def turn_right(self, rate):
+        """
+        Start turning right at the specified rate
+        :param rate: Yaw rate in degrees / second
+        """
+        self.yaw_rate = float(rate)
+
+
+    def simulate(self, time):
+        """
+        Simulate the passing of time
+        :param time: Time in seconds
+        """
+        start_time = self.time
+        end_time = self.time + float(time)
+
+        while self.time < end_time:
+            self._simulate_step(self.STEP_SIZE)
+
+
+    def _simulate_step(self, delta_T):
+        # Update the state
+
+        # We covered some distance
+        self.odometer += self.speed * delta_T
+
+        # To calculate acceleration, we need the weight in newtons, assuming we're on earth:
+        weight_N = self.weight * self.G
+
+        # Calculate longitudinal forces
+        # Roll resistance
+        F_roll = self.MU_N * weight_N
+
+        # Air resistance
+        F_air = 0.5 * self.drag_coefficient * self.frontal_area * self.RHO * pow(self.speed, 2)
+
+        # Engine power, needs to be multiplied by 1k to get watts from kilowatts
+        P_engine = (self.power / 100) * self.engine_power * 1_000
+
+        # Power = Force * Velocity, so Force = Power / Velocity.
+        # To deal with v = 0, let's just say the forward force at V=0 is equal to braking force * power setting
+        if self.speed == 0.0:
+            F_engine = self.brake_force * (self.power / 100)
+        else:
+            F_engine = P_engine / self.speed
+
+        # Brake force
+        F_brake = (self.braking / 100) * self.brake_force
+
+        # Resultant force is: + F_engine - F_air - F_roll - F_brake, where positive is forward
+        force = F_engine - F_air - F_roll - F_brake
+
+        # Force = mass * acceleration, so acceleration = force / mass
+        acceleration = force / weight_N
+
+        # The cars new speed:
+        self.speed += acceleration
+
+        # Deal with turning
+        self.heading += self.yaw_rate
+
+        if self.heading > 360:
+            self.heading -= 360
+
+        self.time += delta_T
--- features/steps/basic_driving.py
+++ features/steps/basic_driving.py
@@ -2,6 +2,7 @@ from behave import *
 
 use_step_matcher('re')
 
+
 @given("that the car is standing still")
 def car_is_standing_still(context):
     context.car.speed = 0
@@ -13,6 +14,31 @@ def step_impl(context, time):
     context.car.simulate(time)
 
 
+@given("the car has (?P<engine_power>\d+) kw, weighs (?P<weight>\d+) kg, has a drag coefficient of (?P<drag>[\.\d]+)")
+def step_impl(context, engine_power, weight, drag):
+    context.car.engine_power = float(engine_power)
+    context.car.weight = float(weight)
+    context.car.drag = float(drag)
+
+
+@given("a frontal area of (?P<area>.+) m\^2")
+def step_impl(context, area):
+    context.car.frontal_area = float(area)
+
+
+@when("I accelerate to (?P<speed>\d+) km/h")
+def step_impl(context, speed):
+    speed_in_ms = float(speed) / 3.6
+    context.car.set_power(100)
+    while context.car.speed < speed_in_ms:
+        context.car.simulate(0.1)
+
+
+@then("the time should be within (?P<precision>[\d\.]+)s of (?P<time>[\d\.]+)s")
+def step_impl(context, precision, time):
+    assert abs(context.car.time - float(time)) < float(precision)
+
+
 @then("the car should be moving")
 def step_impl(context):
     assert context.car.speed > 0
@@ -20,7 +46,7 @@ def step_impl(context):
 
 @given("that the car is moving at (?P<speed>\d+) m/s")
 def step_impl(context, speed):
-    context.car.speed = speed
+    context.car.speed = float(speed)
 
 
 @when("I brake at (?P<brake_force>\d+)% force")
@@ -35,12 +61,12 @@ def step_impl(context, seconds):
 
 @then("I should have traveled less than (?P<distance>\d+) meters")
 def step_impl(context, distance):
-    assert context.car.odometer < distance
+    assert context.car.odometer < float(distance)
 
 
 @given("that the car's heading is (?P<heading>\d+) deg")
 def step_impl(context, heading):
-    context.car.heading = heading
+    context.car.heading = float(heading)
 
 
 @when("I turn (?P<direction>left|right) at a yaw rate of (?P<rate>\d+) deg/sec for (?P<duration>\d+) seconds")
@@ -55,4 +81,4 @@ def step_impl(context, direction, rate, duration):
 
 @then("the car's heading should be (?P<heading>\d+) deg")
 def step_impl(context, heading):
-    assert context.car.heading == heading
+    assert context.car.heading == float(heading)
